bd61ff9aa181ffbe69b7691acc5b22f222a13ef4
Colin Mitchell <muffinista@gmail.com>
2011-11-07 22:06:24 -0500

    custom range implementation in UI

--- api/public/ui.js
+++ api/public/ui.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
+  $("input[name=frequency]").change(function() {
+	console.log("HI!");
+	if ( $("input[name=frequency]:checked").val() == "custom" ) {
+	  $("#min_freq,#max_freq").removeClass("disabled").removeAttr("disabled"); 
+	}
+	else {
+	  $("#min_freq,#max_freq").addClass("disabled").attr("disabled", true); ;
+	}
+
+  });
+
   $('#name-params').submit(function() {
 
 	var opts = {  
-      frequency:  $("input[name=frequency]:checked").val(),
       count:  $("select[name=count]").val(),
       with_surname:  $("input[name=with_surname]:checked").val() === "true"
 	};
+
+    var frequency = $("input[name=frequency]:checked").val();
+	if ( frequency == "custom" ) {
+	  opts.min_freq = $("input[name=min_freq]").val();
+	  opts.max_freq = $("input[name=max_freq]").val();
+	}
+	else {
+	  opts.frequency = frequency;
+	}
+
 
 	var type =  $("input[name=type]:checked").val();
 	if ( type != "both" ) {
