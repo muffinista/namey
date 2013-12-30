ec8bc5efc6bc8fcb6b7f6bedad0bd65fa3e5abbf
Colin Mitchell <muffinista@gmail.com>
2013-12-30 15:51:53 -0500

    jdbc fix

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -2,7 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Namey::Generator" do
   before(:each) do
-    @uri = "#{'jdbc:' if RUBY_PLATFORM == 'java'}#{Namey.db_path}"
+    #@uri = "#{'jdbc:' if RUBY_PLATFORM == 'java'}#{Namey.db_path}"
+    @uri = Namey.db_path
     @gen = Namey::Generator.new(@uri)
   end
 
